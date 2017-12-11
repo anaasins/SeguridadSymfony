9684e2a7bfd036b7bbbc011b48123fb5b65a0471
Ana Asins <asinsanna@gmail.com>
2017-12-11 11:33:47 +0100

    registro usuarios

---
+++ README.md
@@ -0,0 +1,4 @@
+proyectoSeguridad
+=================
+
+A Symfony project created on December 11, 2017, 10:21 am.
---
+++ src/SeguroBundle/Form/UsuarioType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace SeguroBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UsuarioType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('username', TextType::class)
+        ->add('email', EmailType::class)
+        ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repetir Password'),
+            ))
+        ->add('Enviar', SubmitType::class)
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SeguroBundle\Entity\Usuario'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'segurobundle_usuario';
+    }
+
+
+}
